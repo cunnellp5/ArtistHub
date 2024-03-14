f0018fd7657b0a55b8f1e0b649e77beb51cfbfc8
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-14 12:49:09 -0600

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script lang='ts'>
-    import type { PageData } from './$types';
-
-    export let data: PageData;
-</script>
-
 <h1>Artist Hub</h1>
-
-<h2>
-    Showing data from prisma db:
-</h2>
-<ul>
-    {#each data.users as user}
-        <li>
-            {user.name} - {user.email}
-        </li>
-    {/each}
-</ul>
