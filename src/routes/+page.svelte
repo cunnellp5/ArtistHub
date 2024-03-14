7d3158f377ccc590a467b0571603aad9ffe400c3
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-14 12:44:51 -0600

    render prisma data for fun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+<h1>Artist Hub</h1>
+
+<h2>
+    Showing data from prisma db:
+</h2>
+<ul>
+    {#each data.users as user}
+        <li>
+            {user.name} - {user.email}
+        </li>
+    {/each}
+</ul>
